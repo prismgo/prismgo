dbb415ae45f86d7c7565166c335854ca4d908afd
jqh <841324345@qq.com>
2026-06-11 17:30:39 +0800

    mod

--- app/http/controllers/welcome_controller.go
+++ app/http/controllers/welcome_controller.go
@@ -18,8 +18,8 @@ func NewWelcomeController() *WelcomeController {
 // Show returns a small JSON payload proving that the application is running.
 func (c *WelcomeController) Show(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"framework": "prismgo",
-		"name":      config.GetString("app.name", "Prismgo"),
-		"message":   "Welcome to Prismgo.",
+		"framework": "PrismGo",
+		"name":      config.GetString("app.name", "PrismGo"),
+		"message":   "Welcome to PrismGo.",
 	})
 }
